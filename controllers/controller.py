10b996cf3205168e943416acb501ee26f8bf2b6b
Craig <craigwilsonmusic@gmail.com>
2021-04-16 19:45:07 +0100

    First three parts of the MVP completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,13 +2,12 @@ from flask import render_template, request
 from app import app
 from models.player import *
 from models.game import *
-# from models.player import Player
 
 
-@app.route('/<player1>/<player2>')
-def home(player1, player2):
+
+@app.route('/<gesture1>/<gesture2>')
+def home(gesture1, gesture2):
+    player1 = Player("Craig", gesture1)
+    player2 = Player("Jayne", gesture2)
     answer = rps_game(player1, player2)
     return f"{answer}"
-
-    # player1 = Player("Craig", <player1>)
-    # player2 = Player("Jayne", <player2>)
--- models/game.py
+++ models/game.py
@@ -1,18 +1,20 @@
-class Game:
-    pass
+from controllers import *
 
-    # def __init__(self, player1, player2):
-    #     self.player1 = player1
-    #     self.player2 = player2
-valid_gestures = ["rock", "paper", "scissors"]
+
+class Game:
+
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
 
 def rps_game(player1, player2):
-    if (player1 not in valid_gestures) or (player2 not in valid_gestures):
+    valid_gestures = ["rock", "paper", "scissors"]
+    if (player1.gesture not in valid_gestures) or (player2.gesture not in valid_gestures):
         return f"Invalid entry!"
-    elif (player1 == "rock" and player2 == "scissors") or (player1 == "paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
-        return f"Player 1 wins by playing {player1}!"
-    elif (player1 == "rock" and player2 == "paper") or (player1 == "paper" and player2 == "scissors") or (player1 == "scissors" and player2 == "rock"):
-        return f"Player 2 wins by playing {player2}!"
+    elif (player1.gesture == "rock" and player2.gesture == "scissors") or (player1.gesture == "paper" and player2.gesture == "rock") or (player1.gesture == "scissors" and player2.gesture == "paper"):
+        return f"Player 1 wins by playing {player1.gesture}!"
+    elif (player1.gesture == "rock" and player2.gesture == "paper") or (player1.gesture == "paper" and player2.gesture == "scissors") or (player1.gesture == "scissors" and player2.gesture == "rock"):
+        return f"Player 2 wins by playing {player2.gesture}!"
     else:
-        if player1 == player2:
+        if player1.gesture == player2.gesture:
             return None

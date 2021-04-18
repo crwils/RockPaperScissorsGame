f91dd6342d80abe5571b98d1ba83200c54110578
Craig <craigwilsonmusic@gmail.com>
2021-04-18 17:35:54 +0100

    made some changes to tempalte names and page content

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,20 +2,21 @@ from flask import render_template, request
 from app import app
 from models.player import *
 from models.game import *
+import random
 
 @app.route('/home')
 def index():
     return render_template("home.html")
 
-@app.route('/instructions')
+@app.route('/play')
 def instructions():
-    return render_template("instructions.html")
+    return render_template("play.html")
 
-@app.route('/game')
+@app.route('/vs_friend/')
 def game():
-    return render_template("game.html")
+    return render_template("vs_friend.html")
 
-@app.route('/game/<gesture1>/<gesture2>')
+@app.route('/vs_friend/<gesture1>/<gesture2>')
 def game_play_url(gesture1, gesture2):
     player1 = Player("Craig", gesture1)
     player2 = Player("Jayne", gesture2)
@@ -24,22 +25,19 @@ def game_play_url(gesture1, gesture2):
     game1 = Game(player1, player2)
     game2 = Game(player3, player4)
     play = play_game(game1)
-    return render_template("game.html", play=play)
+    return render_template("vs_friend.html", play=play)
 
-# created form on game.html and the app.route below but not finished
-@app.route('/play')
-def game_play():
-    return render_template("play.html")
-
-@app.route('/play', methods=["POST"])
+@app.route('/vs_computer', methods=["GET", "POST"])
 def game_play2():
-    gesture1 = request.form["gesture1"]
-    gesture2 = request.form["gesture2"]
-    name1 = request.form["name1"]
-    name2 = request.form["name2"]
-    player1 = Player(name1, gesture1)
-    player2 = Player(name2, gesture2)
-    game1 = Game(player1, player2)
-    play2 = play_game(game1)
-    return render_template("play.html", play2=play2)
-    # return redirect('/play')
+    if request.method == 'GET':
+        return render_template("vs_computer.html")
+    if request.method == 'POST':
+        gesture1 = request.form["gesture1"]
+        # gesture2 = request.form["gesture2"]
+        name1 = request.form["name1"]
+        # name2 = request.form["name2"]
+        player1 = Player(name1, gesture1)
+        computer = Player("Computer", random.choice(valid_gestures))
+        game1 = Game(player1, computer)
+        play2 = play_game(game1)
+        return render_template("result.html", play2=play2)
--- models/game.py
+++ models/game.py
@@ -6,16 +6,17 @@ class Game:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-
+    
+valid_gestures = ["rock", "paper", "scissors"]
 
 def play_game(game):
     valid_gestures = ["rock", "paper", "scissors"]
     if (game.player1.gesture not in valid_gestures) or (game.player2.gesture not in valid_gestures):
         return f"Invalid entry!"
     elif (game.player1.gesture == "rock" and game.player2.gesture == "scissors") or (game.player1.gesture == "paper" and game.player2.gesture == "rock") or (game.player1.gesture == "scissors" and game.player2.gesture == "paper"):
-        return f"{game.player1.name} wins by playing {game.player1.gesture}!"
+        return f"Result: {game.player1.name} wins by playing {game.player1.gesture}!"
     elif (game.player1.gesture == "rock" and game.player2.gesture == "paper") or (game.player1.gesture == "paper" and game.player2.gesture == "scissors") or (game.player1.gesture == "scissors" and game.player2.gesture == "rock"):
-        return f"{game.player2.name} wins by playing {game.player2.gesture}!"
+        return f"Result: {game.player2.name} wins by playing {game.player2.gesture}!"
     else:
         if game.player1.gesture == game.player2.gesture:
             return None
